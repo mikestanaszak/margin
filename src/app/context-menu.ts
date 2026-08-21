afbe0c6cd9350582ba253aa9bdbaee982160f037
mikestanaszak <mikestanaszak@users.noreply.github.com>
2026-08-21 08:46:00 -0500

    Normalize-context-menu-selector

--- src/app/context-menu.ts
+++ src/app/context-menu.ts
@@ -1,6 +1,6 @@
 export function shouldSuppressWebviewContextMenu(target: EventTarget | null) {
   return !(
     target instanceof Element &&
-    target.closest('.cm-content[contenteditable="true"][spellcheck="true"]')
+    target.closest(".cm-content[contenteditable='true'][spellcheck='true']")
   );
 }
